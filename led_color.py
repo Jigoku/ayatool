e209df82bbefbccb5baf9824367035eba99bb100
Ricky Thomson <flytheflag@gmx.co.uk>
2017-04-03 11:41:37 +0100

    tidy up

--- led_color.py
+++ led_color.py
@@ -8,9 +8,8 @@ if os.geteuid() != 0:
 	exit("You need to have root privileges to run this script.")
 
 blackweb_aya.open_usb()
-blackweb_aya.change_color(sys.argv[1],sys.argv[2],sys.argv[3])
 blackweb_aya.change_mode(2)
+blackweb_aya.change_color(sys.argv[1],sys.argv[2],sys.argv[3])
 #blackweb_aya.change_polling(3)
 blackweb_aya.store_settings()
-#blackweb_aya.debug()
 blackweb_aya.close_usb()
