2d9af13f8f21e0e42a9fda6106d86deed6654ab9
Ricky Thomson <flytheflag@gmx.co.uk>
2017-04-13 00:19:09 +0100

    move designer ui file to its own folder

--- ayatoolgui.py
+++ ayatoolgui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ayatool.ui'
+# Form implementation generated from reading ui file 'qtdesigner/ayatool.ui'
 #
 # Created by: PyQt4 UI code generator 4.12
 #
